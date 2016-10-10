9dc9ba932ad2af9f301390279f32985f74b954f1
Piji You <you_peijie@yahoo.com>
2016-10-10 22:38:35 +0800

    Simple news board.
    Todo:
    1.	change css
    2.	add related api

---
+++ news.js
@@ -0,0 +1,40 @@
+var NewsData = {
+	userName	: "user1",
+	userPhoto	: "http://tse3.mm.bing.net/th?id=OIP.M85a3ed107ce53ffb158b1912965da846o0&w=78&h=78&c=7&pid=1.1",
+	pictureUrl	: "http://www.bing.com/th?id=OJ.Pg74LEGQi7DZ6A&pid=MSNJVFeeds",
+	location	: "ZJ",
+	date		: new Date()
+	//countLike	: 0;
+	//countComment : 0;
+};
+
+//	for demo
+var news1 = Object.create(NewsData);
+var news2 = Object.create(NewsData);
+var news3 = Object.create(NewsData);
+var news = [news1, news2, news3];
+
+var News = React.createClass({
+	render : function() {
+		return (
+		<div>
+		{
+			this.props.data.map(function(data) {
+				return (
+				<div style={{marginTop: "20px", backgroundColor: "bisque"}}>
+					<p>{data.userName}</p>
+					<img src={data.userPhoto}/>
+					<img src={data.pictureUrl}/>
+					<p>{data.location}</p>
+					<p>{data.date.toString()}</p>
+				</div>);
+			})
+		}
+		</div>);
+	}
+});
+
+ReactDOM.render(
+	<News data={news}></News>,
+	document.getElementById('content')
+);
