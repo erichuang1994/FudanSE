4c8f898ec7b3ed052c158528839cfd9a6fabd833
eric <q465168867@gmail.com>
2016-10-11 16:47:45 +0800

    fixed user photo

--- app/src/News.js
+++ app/src/News.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-// import './App.css';
-
+import './News.css'
+import 'purecss'
+import 'purecss/build/grids-responsive-min.css';
+// import { Button, Cell } from 'react-pure';
+// class Card extends Component {
+// 	render(){
+// 				return (
+// 				<div key={data.id} className="pure-u-1 pure-u-md-1-3">
+// 					<p>{data.userName}</p>
+// 					<img alt={data.pictureUrl} src={data.userPhoto}/>
+// 					<img className="pure-img" alt={data.pictureUrl} src={data.pictureUrl}/>
+// 					<p>{data.location}</p>
+// 					<p>{data.date.toString()}</p>
+// 				</div>);
+// 		);
+// 	}
+// }
 class News extends Component {
 	render(){
 		return (
-		<div>
+		<div className="pure-g">
 		{
 			this.props.data.map(function(data) {
 				return (
-				<div style={{marginTop: "20px", backgroundColor: "bisque"}}>
-					<p>{data.userName}</p>
-					<img src={data.userPhoto}/>
-					<img src={data.pictureUrl}/>
-					<p>{data.location}</p>
+				<div key={data.id} className="pure-u-1 pure-u-md-1-3">
+					<div className="pure-g">
+					<div className="pure-u-1-3">
+					<a href={data.userlink}>
+						<img className="size-xs" alt={data.pictureUrl} src={data.userPhoto}/>
+					</a>
+					</div>
+					<div className="pure-u-2-3">
+					<ul className="pure-menu-list">
+		 		 		<li className="pure-menu-item">{data.userName}</li>
+		 				<li className="pure-menu-item">{data.location}</li>
+ 	 				</ul>
+					</div>
+					</div>
+					<img className="pure-img" alt={data.pictureUrl} src={data.pictureUrl}/>
 					<p>{data.date.toString()}</p>
 				</div>);
 			})
--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import News from './News'
+import Topbar from './Topbar'
 import './index.css';
-var NewsData = {
+var NewsData = function(id){return {
+  userlink:"u/12345",
+  id:id,
 	userName	: "user1",
 	userPhoto	: "http://tse3.mm.bing.net/th?id=OIP.M85a3ed107ce53ffb158b1912965da846o0&w=78&h=78&c=7&pid=1.1",
 	pictureUrl	: "http://www.bing.com/th?id=OJ.Pg74LEGQi7DZ6A&pid=MSNJVFeeds",
@@ -11,15 +14,17 @@ var NewsData = {
 	date		: new Date()
 	//countLike	: 0;
 	//countComment : 0;
-};
+}};
 
 //	for demo
-var news1 = Object.create(NewsData);
-var news2 = Object.create(NewsData);
-var news3 = Object.create(NewsData);
+var news1 = NewsData(1);
+var news2 = NewsData(2);
+var news3 = NewsData(3);
 var news = [news1, news2, news3];
+var genMenus = function(name,href){return {name:name,href:href}};
+var menus = [genMenus("广场","news"),genMenus("消息","messgae")];
 ReactDOM.render(
-  <App />,
+  <Topbar menus={menus}/>,
   document.getElementById('root')
 );
 ReactDOM.render(
