99affd8ddb1752d33984059b7d398123978dc745
eric <q465168867@gmail.com>
2016-10-11 19:15:34 +0800

    update

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import News from './News'
-import Topbar from './Topbar'
+import News from './News';
+import Topbar from './Topbar';
 import './index.css';
 var NewsData = function(id){return {
   userlink:"u/12345",
