fc23e2efcd6bdcef8d1a46e64cf4821de3fad0b3
HuangHengyi <henrymakaay@gmail.com>
2016-10-12 10:54:01 +0800

    123

--- app/src/Signup.js
+++ app/src/Signup.js
@@ -12,13 +12,13 @@ class Signup extends Component {
             <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
             </source>
         </video>
-        <div className="inner-container">
+        <div className="innercontainer">
           <video id="Video2" className="bgvid inner" autoPlay="false" muted="muted" preload="auto" loop>
             <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
             </source>
           </video>
           <div className="box">
-            <h1>Login</h1>
+            <h1>Sign up</h1>
             <input type="text" placeholder="Username"/>
             <input type="text" placeholder="Password"/>
             <input type="text" placeholder="Nickname"/>
