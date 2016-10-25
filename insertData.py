6dc260ca4952b304a5d8a7b11758dff7d06898dc
xhmFlydutchman <flydutchman@foxmail.com>
2016-10-25 22:45:53 +0800

    Add visited city related functionalities.

--- insertData.py
+++ insertData.py
@@ -4,9 +4,9 @@ django.setup()
 from travelMap.models import *
 from django.contrib.auth.models import User
 
-user1 = User.objects.create_user(username='Haoming', password='123123', email='wow@such.doge')
+if __name__ == "__main__":
+	user1 = User.objects.create_user(username='Haoming', password='123123', email='wow@such.doge')
 
-city1 = City.objects.create(name='Shanghai')
-city2 = City.objects.create(name='Hongkong')
-city3 = City.objects.create(name='Washington')
-
+	city1 = City.objects.create(name='Shanghai')
+	city2 = City.objects.create(name='Hongkong')
+	city3 = City.objects.create(name='Washington')
--- travelMap/apiHandlers.py
+++ travelMap/apiHandlers.py
@@ -11,50 +11,66 @@ from travelMap.models import *
 
 # Handlers for API calls from frontend
 
+def getOnlyElement(a):
+    if (len(a) != 1) :
+        return None;
+    return a[0]
+
 @csrf_exempt
 def signup(request):
     # username, password, email
-    username = request.POST['username']
-    password = request.POST['password']
-    email = request.POST['email']
-    print(request.POST)
-    if (User.objects.filter(username=username).count() > 0):
-        print(User.objects.filter(username=username))
-        return HttpResponseBadRequest('Username already exist!')
-    user = User.objects.create_user(username=username, password=password, email=email)
-    Traveller.objects.create(user=user)
-    auth.login(request, user)
-    return HttpResponse()
+    if (request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        if (User.objects.filter(username=username).count() > 0):
+            print(User.objects.filter(username=username))
+            return HttpResponseBadRequest('Username already exist!')
+        user = User.objects.create_user(username=username, password=password, email=email)
+        Traveller.objects.create(user=user)
+        auth.login(request, user)
+        return HttpResponse()
+    return HttpResponseNotFound()
     
 @csrf_exempt
 def login(request):
     # username, password
-    username = request.POST['username']
-    password = request.POST['password']
-    user = auth.authenticate(username=username, password=password)
-    if user is not None:
-        auth.login(request, user)
-        return HttpResponse()
-    else:
-        return HttpResponse('username or password incorrect!', status=401)
+    if (request.method == 'POST') :
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponse()
+        else:
+            return HttpResponse('username or password incorrect!', status=401)
+    return HttpResponseNotFound()
 
 @csrf_exempt
 @login_required
 def logout(request):
-    auth.logout(request)
-    return HttpResponse()
+    if (request.method == 'POST'):
+        auth.logout(request)
+        return HttpResponse()
+    return HttpResponseNotFound()
 
 @csrf_exempt
 @login_required
 def comments_of_picture(request, picture_id):
-    pass
+    if (request.method == 'GET'):
+        picture = getOnlyElement(
+            Picture.objects.filter(id=picture_id));
+        if (picture is None) :
+            return HttpResponseNotFound("No Such Picture!");
+        messages = Message.objects.filter(picture=picture)
+        return HttpResponse(messages);
+    return HttpResponseNotFound()
 
 @csrf_exempt
 @login_required
 def modify_setting(request):
     # password
-    username = request.user.get_username();
-    user = User.objects.filter(username=username)[0];
+    user = request.user;
     user.set_password(request.POST['password'])
     user.save()
     return HttpResponse()
@@ -62,36 +78,45 @@ def modify_setting(request):
 @csrf_exempt
 @login_required
 def add_visited(request, cityname):
-    # cityname
-    return HttpResponse(cityname)
+    # cityname / POST
+    if (request.method == 'POST'):
+        city = getOnlyElement(City.objects.filter(name=cityname))
+        if (city is None) :
+            return HttpResponseNotFound("No Such City")
+        traveller = getOnlyElement(Traveller.objects.filter(user=request.user));
+        traveller.follows.add(city);
+        return HttpResponse()
+    return HttpResponseNotFound()
+
+@csrf_exempt
+@login_required
+def visited_cities(request, username):
+    if (request.method == 'GET'):
+        traveller = getOnlyElement(user=request.user)
+        return HttpResponse(traveller.cities)
+    return HttpResponseNotFound()
 
 @csrf_exempt
 @login_required
 def upload(request, cityname):
-    # image file
+    # image file / POST
     pass
 
 @csrf_exempt
 @login_required
 def update_followers(request):
-    # username
+    # username / POST
     pass
 
 @csrf_exempt
 @login_required
 def messages_received(request):
-    # username
+    # username / POST
     pass
 
 @csrf_exempt
 @login_required
 def get_traverller(request, username):
-    # TODO
-    pass
-
-@csrf_exempt
-@login_required
-def visited_cities(request, username):
     # TODO
     pass
 
