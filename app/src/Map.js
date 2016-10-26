b570f2b7d0c7d7af8f2214ffca855fc466677e56
eric <q465168867@gmail.com>
2016-10-26 13:37:12 +0800

    加了添加城市的按钮 没时间改样式了

--- app/src/Map.js
+++ app/src/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import News from './News';
 import './Map.css';
+import './base.css';
 import Addpic from './Addpic';
 import {newsdata,shanghai,yangzhou} from './newsdata';
 import {
@@ -48,6 +49,27 @@ var Map = React.createClass({
 	addpic:function(){
 	  this.setState({addPic: true});
 	},
+	addCity:function(){
+		this.setState({addCity: true});
+	},
+	handleAddCity:function(){
+		// fetch("/api/", {
+		// 	credentials: 'include',
+	  //   method: 'post',
+	  //   body: data
+	  // })
+		// .then(function(res){
+  	// if(res.status === 200){
+    // 	loginfunc();
+  	// }else if(res.status === 401){
+    // 	alert("密码错误");
+  	// }});
+		this.setState({addCity:false});
+	},
+	cancelAddCity:function(){
+
+		this.setState({addCity:false});
+	},
 	closeAddPic:function(){
 	  this.setState({addPic: false});
 	},
@@ -68,41 +90,58 @@ var Map = React.createClass({
 		this.setState({focusCity: name});
 	},
 	render : function() {
+		var style1={
+				display: 'block',
+				position: 'fixed',
+				backgroundColor:'rgba(0, 0, 0, 0.498039)'
+			};
 		return (
 			<div>
-				{this.state.addPic ? (
-					<div className="ale">
-						<Addpic></Addpic>
-						<button className="upl" onClick={this.closeAddPic}>确认上传</button>
+				{this.state.addCity ? (
+					<div className="ux-popmenu" style={style1}>
+						<div className="fixed-center" >
+        			<input className="base-input" ref="cityname" type="text" placeholder="输入城市名"/>
+        			<button  className="base-button pure-button pure-button-primary" onClick={this.handleAddCity}>添加</button>
+							<button  className="base-button pure-button pure-button-primary" onClick={this.cancelAddCity}>取消</button>
+						</div>
 					</div>
 				) :
 				null}
-				<div className="mapbox">
-					<button>添加城市</button>
+				<div>
+					{this.state.addPic ? (
+						<div className="ale">
+							<Addpic></Addpic>
+							<button className="upl" onClick={this.closeAddPic}>确认上传</button>
+						</div>
+					) :
+					null}
+					<div className="mapbox">
+						<button onClick={this.addCity}>添加城市</button>
+					</div>
+					<div className="mapbox">
+						<button onClick={this.addpic}>添加照片</button>
+					</div>
+				<MapWithMarker
+					zoom={5}
+					mapCenter={{ lat: 26.08, lng: 119.3 }}
+					marker={[MarkerData('上海', 31.2304, 121.4737), MarkerData('香港', 22.2, 114.1), MarkerData('扬州', 32.39, 119.42)]}
+					containerElement={
+						<div style={{ height: this.height+'px', width: this.width+'px', maxWidth: '100%'}} />
+					}
+					mapElement={
+						<div style={{ height: this.height+'px', width: this.width+'px', maxWidth: '100%'}} />
+					}
+					updateFocusCity={this.updateFocusCity}
+				/>
+				{this.state.focusCity==="上海" ? (
+				<News data={shanghai} deleteById={()=>{console.log("hello world");}}></News>
+				) :
+				null}
+				{this.state.focusCity==="扬州" ? (
+				<News data={yangzhou} deleteById={()=>{console.log("hello world");}}></News>
+				) :
+				null}
 				</div>
-				<div className="mapbox">
-					<button onClick={this.addpic}>添加照片</button>
-				</div>
-			<MapWithMarker
-				zoom={5}
-				mapCenter={{ lat: 26.08, lng: 119.3 }}
-				marker={[MarkerData('上海', 31.2304, 121.4737), MarkerData('香港', 22.2, 114.1), MarkerData('扬州', 32.39, 119.42)]}
-				containerElement={
-					<div style={{ height: this.height+'px', width: this.width+'px', maxWidth: '100%'}} />
-				}
-				mapElement={
-					<div style={{ height: this.height+'px', width: this.width+'px', maxWidth: '100%'}} />
-				}
-				updateFocusCity={this.updateFocusCity}
-			/>
-			{this.state.focusCity==="上海" ? (
-			<News data={shanghai} deleteById={()=>{console.log("hello world");}}></News>
-			) :
-			null}
-			{this.state.focusCity==="扬州" ? (
-			<News data={yangzhou} deleteById={()=>{console.log("hello world");}}></News>
-			) :
-			null}
 			</div>
 		);
 	}
