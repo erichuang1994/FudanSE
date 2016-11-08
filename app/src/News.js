e11e33c1dd48240dd155145926bec5c30628875a
Piji You <you_peijie@yahoo.com>
2016-11-08 16:23:03 +0800

    Seperate card component.

--- app/src/News.js
+++ app/src/News.js
@@ -7,18 +7,79 @@ import './base.css';
 import './cards.css';
 import './mods.css';
 
-var clickLike = function(dom, index) {
-  console.log('like');
-  console.log(index);
-}
-
-var clickComment = function(dom, index) {
-  var commentBox = dom.commentBox[index];
-  commentBox.hidden = !commentBox.hidden;
-  if (!commentBox.hidden) {
-    // add get comment here
+var NewsComment = React.createClass({
+  render: function() {
+    return (<div>1</div>);
   }
-}
+});
+
+var Card = React.createClass({
+  getInitialState: function() {
+    return {comment: false};
+  },
+
+  clickLike: function() {
+  },
+
+  clickComment: function() {
+    if (!this.state.comment) {
+	  var commentData = [1];
+	  this.setState({comment: true, commentData:[1]})
+	}
+	else {
+	  this.setState({comment: false});
+	}
+  },
+
+  render : function() {
+    var card = this.props.data;
+	if (this.state.comment) {
+	  var commentBox = (
+        <div className='comment-box'>
+		  {
+  		    this.state.commentData.map((data, index) => {
+		      return (<NewsComment key={index} data={data} />);
+		    })
+		  }
+        </div>
+	  );
+	}
+    return (
+	  <div key={this.props.index} className="card card9 line-around" >
+	    <header className="layout-box media-graphic">
+          <div className="mod-media size-xs news-header">
+            <a href={this.props.userlink}>
+              <img className="size-xs" alt={card.pictureUrl} src={card.userPhoto}/>
+            </a>
+          </div>
+          <div className="box-col item-list">
+            <a className="item-main txt-l mct-a txt-cut"><span>{card.userName}</span></a>
+            <div className="item-minor txt-xxs mct-d txt-cut">
+              <span className="time">{card.date.toLocaleDateString("ch-CN")}</span>
+              <span className="from">来自{card.location}</span>
+            </div>
+          </div>
+          <a className="operate-box"><i className="icon-font icon-font-arrow-down txt-s" onClick={this.props.editfunc}></i></a>
+        </header>
+        <div className="news-body">
+          <img className="pure-img" alt={card.pictureUrl} src={card.pictureUrl}/>
+        </div>
+        <ul className="news-toolbar">
+          <li className="news-tool">
+            <a href="javascript:;" onClick={this.clickLike}>
+              0赞
+            </a>
+          </li>
+          <li className="news-tool">
+            <a href="javascript:;" onClick={this.clickComment}>
+              0评论
+            </a>
+          </li>
+        </ul>
+		{commentBox}
+      </div>);
+  }
+});
 
 var News = React.createClass({
   getInitialState:function() {
@@ -44,7 +105,7 @@ var News = React.createClass({
 			var style1={
 					display: 'block',
 				 	position: 'fixed',
-				  backgroundColor:'rgba(0, 0, 0, 0.498039)'
+					backgroundColor:'rgba(0, 0, 0, 0.498039)'
 				};
 			var popup = (
 				<div className="ux-popmenu" style={style1}>
@@ -70,41 +131,8 @@ var News = React.createClass({
 			{
 				this.props.data.map((card, index) => {
 					return (
-					<div key={index} className="card card9 line-around" >
-						<header className="layout-box media-graphic">
-							<div className="mod-media size-xs news-header">
-								<a href={card.userlink}>
-									<img className="size-xs" alt={card.pictureUrl} src={card.userPhoto}/>
-								</a>
-							</div>
-							<div className="box-col item-list">
-			 		 				<a className="item-main txt-l mct-a txt-cut"><span>{card.userName}</span></a>
-									<div className="item-minor txt-xxs mct-d txt-cut">
-										<span className="time">{card.date.toLocaleDateString("ch-CN")}</span>
-										<span className="from">来自{card.location}</span>
-									</div>
-							</div>
-							<a className="operate-box"><i className="icon-font icon-font-arrow-down txt-s"  onClick={() => this.editfunc(index)}></i></a>
-						</header>
-						<div className="news-body">
-							<img className="pure-img" alt={card.pictureUrl} src={card.pictureUrl}/>
-						</div>
-						<ul className="news-toolbar">
-							<li className="news-tool">
-								<a href="javascript:;" onClick={() => {clickLike(this, index)}}>
-									0赞
-								</a>
-							</li>
-							<li className="news-tool">
-								<a href="javascript:;" onClick={() => {clickComment(this, index)}}>
-									0评论
-								</a>
-							</li>
-						</ul>
-						<div className='comment-box' hidden={true} ref={(commentBox => this.commentBox.push(commentBox))}>
-							1
-						</div>
-					</div>);
+						<Card key={index} data={card} index={index} editfunc={() => {this.editfunc(index)}}/>
+					);
 				})
 			}
 			</div>
